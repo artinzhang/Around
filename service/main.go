39eb70c78764f2879bbb0e921fdbf34e4e261715
Artin <artinzhang7@gmail.com>
2019-05-01 09:23:50 -0700

    update

--- service/main.go
+++ service/main.go
@@ -47,6 +47,10 @@ func main() {
 		panic(err)
 		return
 	}
+	if err != nil {
+		panic(err)
+		return
+	}
 
 	// Use the IndexExists service to check if a specified index exists.
 	exists, err := client.IndexExists(INDEX).Do() // Check if the index exists.
